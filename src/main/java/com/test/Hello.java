bc0eb78923bcd39b8e3dfb8a122a12bbd1a65660
Xb95395 <jinzhaoran@163.com>
2019-03-12 18:45:32 +0800

    555

--- src/main/java/com/test/Hello.java
+++ src/main/java/com/test/Hello.java
@@ -1,10 +1,10 @@
 package com.test;
 
 public class Hello {
-
 	public static void main(String[] args) {
 		System.out.println("123");
 		System.out.println("第二次修改");
 		System.out.println("132456");
 	}
 }
+
